ff21624f88fea01c6007bb1e74fa473b54b9e055
znmic <zx520123123>
2019-10-11 09:06:29 +0800

    屏幕翻转，不从0开始

--- app/src/main/java/com/example/myapplicationnull/MainActivity.java
+++ app/src/main/java/com/example/myapplicationnull/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplicationnull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private Button mNextButton;
     private Button mPreButton;
     private TextView mQuestionTextView;
+    private static final String KEY_INDEX="index";
+    private static final String TAG="MainActivity";
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.Q1,true),
             new Question(R.string.Q2,true),
@@ -26,7 +29,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState!=null){
+            mCountIndex=savedInstanceState.getInt(KEY_INDEX);
+        }
 
         mQuestionTextView=findViewById(R.id.question_text_view);
 
@@ -65,6 +73,43 @@ public class MainActivity extends AppCompatActivity {
 
         updateQuestion();
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG,"onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX,mCountIndex);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
+
+
+
 
     private void updateQuestion(){
         int question=mQuestionBank[mCountIndex].getTextResId();
